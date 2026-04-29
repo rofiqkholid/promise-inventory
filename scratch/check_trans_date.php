fb6efac8e43aab8290e04cee84cf5a074b92ab3f
Reza Satria Pangestu <rezasatriapangestu@gmail.com>
2026-04-29 07:59:14 +0700

    Rollback seluruh proyek ke versi 2f0ab81

--- scratch/check_trans_date.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-use Illuminate\Support\Facades\DB;
-
-$t = DB::table('inv_t_inventory_transaction')->where('id', 6)->first();
-echo "Transaction 6 date: {$t->transaction_date}" . PHP_EOL;
--- scratch/check_vave_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-use Illuminate\Support\Facades\DB;
-
-$sample = DB::table('inv_m_vave_base')->orderBy('id', 'desc')->first();
-print_r($sample);
